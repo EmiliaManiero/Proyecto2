9c12f33fc46825978d22102608ed8d31a6031185
EmiliaManiero <108247083+EmiliaManiero@users.noreply.github.com>
2022-08-30 14:01:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-//Sumar Iva a producto
-function CalcularIVA (precioCosto){
-    return (precioCosto * 0.21)
+function Limpiar() {
+    document.getElementById('miformulario').reset();
 }
-var iva = CalcularIva(precioCosto);
-ivaM= iva.toFixed(2);
-
-alert("El IVA del producto es: "+ivaM);
-console.log("El IVA del producto es: "+ivaM);
+function sumar() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x+y;
+}
+function restar() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x-y;
+}
+function multiplicar() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x*y;
+}
+function dividir() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x/y;
+}
