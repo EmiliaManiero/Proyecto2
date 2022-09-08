bc0ac11d3998aafb7bcca1c2fef88fd0cb7d5920
EmiliaManiero <108247083+EmiliaManiero@users.noreply.github.com>
2022-09-08 20:52:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const doctorStrange = {nombre:doctorStrange, clasificacion:APT, precio:800}
-const Minions = {nombre:Minions, clasificacion:APT, precio:500}
-const ForrestGump = {nombre:ForrestGump, clasificacion:SAM16, precio:800}
-const Scarface = {nombre:Sccarface, clasificacion:SAM18, precio:800}
-const Batman = {nombre:Batman, clasificacion:SAM16, precio:800}
-const TheSuicideSquad = {nombre:TheSuicideSquad, clasificacion:SAM18, precio:700}
+const doctorStrange = {nombre:"doctorStrange", clasificacion:"APT", precio:800}
+const Minions = {nombre:"Minions", clasificacion:"APT", precio:500}
+const ForrestGump = {nombre:"ForrestGump", clasificacion:"SAM16", precio:800}
+const Scarface = {nombre:"Scarface", clasificacion:"SAM18", precio:800}
+const Batman = {nombre:"Batman", clasificacion:"SAM16", precio:800}
+const TheSuicideSquad = {nombre:"TheSuicideSquad", clasificacion:"SAM18", precio:700}
 
 
 let numeros = [1,2,3,4,5]
